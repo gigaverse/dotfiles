9f85969e7a56de05723fb39bbc9681ee3102eef8
Youssef Mahmoud <yam160130@utdallas.edu>
2020-06-18 12:05:16 -0500

    updates

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -17,14 +17,14 @@ dracula.draw.blood(c, {
 # Uncomment this to still load settings configured via autoconfig.yml
 #config.load_autoconfig()
 
-c.url.start_pages = ["file:///home/youssef/Projects/StartPage/homepage.html"]
+c.url.start_pages = ["file:///home/gigaverse/proj/StartPage/homepage.html"]
 config.set('tabs.padding', { 'top': 5, 'bottom': 5, 'left': 5, 'right': 5 })
 
 c.colors.webpage.prefers_color_scheme_dark = True
 
 # setting, it's replaced with the fonts listed here.
 # Type: Font
-c.fonts.default_family = ["Source Code Pro Semibold", "IBM Plex Mono", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", "Liberation Mono"]
+c.fonts.default_family = ["mononoki", "IBM Plex Mono", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", "Liberation Mono"]
 
 # Font family for standard fonts.
 # Type: FontFamily
@@ -34,54 +34,54 @@ c.fonts.web.family.serif =  '"Tinos For Powerline", "Roboto Condensed", serif'
 
 # Font used in the completion widget.
 # Type: Font
-c.fonts.completion.entry = '9pt xos4 Terminess Powerline'
+c.fonts.completion.entry = '9pt mononoki'
 
 # Font used in the completion categories.
 # Type: Font
-c.fonts.completion.category = 'bold 9pt xos4 Terminess Powerline'
+c.fonts.completion.category = 'bold 9pt mononoki'
 
 # Font used for the debugging console.
 # Type: QtFont
-c.fonts.debug_console = '9pt xos4 Terminess Powerline'
+c.fonts.debug_console = '9pt mononoki'
 
 # Font used for the downloadbar.
 # Type: Font
-c.fonts.downloads = '9pt xos4 Terminess Powerline'
+c.fonts.downloads = '9pt mononoki'
 
 # Font used for the hints.
 # Type: Font
-c.fonts.hints = 'bold 9pt xos4 Terminess Powerline'
+c.fonts.hints = 'bold 9pt mononoki'
 
 # Font used in the keyhint widget.
 # Type: Font
-c.fonts.keyhint = '9pt xos4 Terminess Powerline'
+c.fonts.keyhint = '9pt mononoki'
 
 # Font used for error messages.
 # Type: Font
-c.fonts.messages.error = '9pt xos4 Terminess Powerline'
+c.fonts.messages.error = '9pt mononoki'
 
 # Font used for info messages.
 # Type: Font
-c.fonts.messages.info = '9pt xos4 Terminess Powerline'
+c.fonts.messages.info = '9pt mononoki'
 
 # Font used for warning messages.
 # Type: Font
-c.fonts.messages.warning = '9pt xos4 Terminess Powerline'
+c.fonts.messages.warning = '9pt mononoki'
 
 # Font used for prompts.
 # Type: Font
-c.fonts.prompts = '12pt Source Code Pro'
+c.fonts.prompts = '12pt mononoki'
 
 # Font used in the statusbar.
 # Type: Font
-c.fonts.statusbar = '9pt Source Code Pro'
+c.fonts.statusbar = '9pt mononoki'
 
 # Font used in the tab bar.
 # Type: QtFont
-c.fonts.tabs = '10pt "Source Code Pro"'
+c.fonts.tabs = '10pt "mononoki"'
 
 #SEARCH ENGINES
-c.url.searchengines = {"DEFAULT": "https://duckduckgo.com/?q={}",
+c.url.searchengines = {"DEFAULT": "https://searx.ninja/?q={}",
                        "wa": "https://wiki.archlinux.org/?search={}",
                        "rym":"https://rateyourmusic.com/search?bx=00a2c8ac73aa342923683822e7a86878&searchtype=a&searchterm={}",
                        "rymr": "https://rateyourmusic.com/search?bx=d0ac6eb2977e79320ac0caafc4e5dfbf&searchtype=l&searchterm={}"
